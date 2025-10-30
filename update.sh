3755ba0d85898e5f29617786e80bc0b30e8071ba
quochuydev <quochuy.dev@gmail.com>
2025-10-30 11:06:16 +0700

    update

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 # Script Vars
 REPO_URL="https://github.com/quochuydev/x-app-v1.git"
 APP_DIR=~/myapp
+BRANCH="main"
+
+echo "Branch: $BRANCH"
 
 # Pull the latest changes from the Git repository
 if [ -d "$APP_DIR" ]; then
@@ -14,6 +17,8 @@ else
   git clone $REPO_URL $APP_DIR
   cd $APP_DIR
 fi
+
+git checkout $BRANCH
 
 # Build and restart the Docker containers from the app directory (~/myapp)
 echo "Rebuilding and restarting Docker containers..."
