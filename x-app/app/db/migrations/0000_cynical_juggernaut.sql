e1a7d37b286fc5f02fa058b52045f5689df6cfa4
quochuydev <quochuy.dev@gmail.com>
2025-11-12 14:40:32 +0700

    remove src

--- x-app/app/db/migrations/0000_cynical_juggernaut.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "todos" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"content" varchar(255) NOT NULL,
-	"completed" boolean DEFAULT false,
-	"created_at" timestamp DEFAULT now()
-);
